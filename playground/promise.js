f5755448c85b107cd6b7ff6c7607d0ef58bb031b
Sachin Aggarwal <sachinaggarwal@Sachins-MacBook-Air.local>
2018-01-12 12:53:52 +0530

    promises actual use with arguments and abstraction

--- playground/promise.js
+++ playground/promise.js
@@ -1,13 +1,18 @@
-var somePromise = new Promise((resolve,reject)=>{
-  // resolve('hey it worked');
-  reject('Unable to fulfill promise');
-});
+//this is a function that will create our request just like getWeather() function
+
+var asychAddusingPromise = (a,b)=>{
+  return new Promise((resolve,reject)=>{
+    //your request here which will take some time
+    setTimeout(()=>{
+      if(typeof a === 'number' && typeof b === 'number')
+      resolve(a+b);
+      else
+      reject('Arguments must be numbers');
+    }, 1500);
+  });
+};
 
 
-somePromise.then(
-  (message)=>{
-  console.log('Sucess:',message);
-},
-(error)=>{
-  console.log('Error:',error);
-});
+//now lets see how we can use getWeather in app.js....here lets show for add =>
+asychAddusingPromise(5,7).then( (result)=>{console.log(result);} , (error)=>{console.log(error);} );
+//=> as you can see the promise has reduced everything to 1 line instead of all if else => we have done abstraction
