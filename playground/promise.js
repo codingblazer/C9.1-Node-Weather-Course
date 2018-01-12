a5be34970bbb365339bf8c8a2309d72e871a7639
Sachin Aggarwal <sachinaggarwal@Sachins-MacBook-Air.local>
2018-01-12 16:21:05 +0530

    chaining of promises

--- playground/promise.js
+++ playground/promise.js
@@ -12,7 +12,29 @@ var asychAddusingPromise = (a,b)=>{
   });
 };
 
+var asychAddusingPromise2 = (a,b)=>{
+  return new Promise((resolve,reject)=>{
+    //your request here which will take some time
+    setTimeout(()=>{
+      if(typeof a === 'number' && typeof b === 'number')
+      resolve(a+b);
+      else
+      reject('Arguments must be numbers');
+    }, 1500);
+  });
+};
+
 
 //now lets see how we can use getWeather in app.js....here lets show for add =>
-asychAddusingPromise(5,7).then( (result)=>{console.log(result);} , (error)=>{console.log(error);} );
+//asychAddusingPromise(5,7).then( (result)=>{console.log(result);} , (error)=>{console.log(error);} );
 //=> as you can see the promise has reduced everything to 1 line instead of all if else => we have done abstraction
+
+asychAddusingPromise(5,'7').then(
+  (result)=>{
+    console.log(result);
+    return asychAddusingPromise2(result,33);
+  }).then((res)=>{
+    console.log(res);
+  }).catch((error)=>{
+    console.log(error);
+  });
